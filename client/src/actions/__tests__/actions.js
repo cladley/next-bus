27c74503c95ce14e75145fe5198f15f3a9bf959b
Colin Ladley <colinladley@hotmail.com>
2018-11-29 20:35:27 +0000

    Add tests for action creator to fetch stops by location

---
+++ client/src/actions/__tests__/actions.js
@@ -0,0 +1,46 @@
+import { fetchStopsByLocation, setStopMarkers } from "../index";
+import * as actionTypes from "../actionTypes";
+
+describe("Action creators", () => {
+  describe("fetchStopsByLocation()", () => {
+    it("should return object in correct format", () => {
+      const coordinates = {
+        lat: 42.424,
+        lon: -0.23443
+      };
+
+      const expectedObject = {
+        type: actionTypes.API,
+        payload: {
+          url: `https://api.tfl.gov.uk/Stoppoint?app_id=6434337f&app_key=ddf7e98f6e48334e7efd30c2cbd9c483&lat=${
+            coordinates.lat
+          }&lon=${
+            coordinates.lon
+          }&stoptypes=NaptanPublicBusCoachTram&radius=300`,
+          onSuccess: setStopMarkers
+        }
+      };
+
+      expect(fetchStopsByLocation(coordinates.lat, coordinates.lon)).toEqual(
+        expectedObject
+      );
+    });
+  });
+
+  describe("setStopMarkers()", () => {
+    it("should return object in correct format", () => {
+      const data = {
+        stopPoints: {}
+      };
+
+      const expectedObject = {
+        type: actionTypes.SET_STOP_MARKERS,
+        payload: {
+          data: data.stopPoints
+        }
+      };
+
+      expect(setStopMarkers(data)).toEqual(expectedObject);
+    });
+  });
+});
