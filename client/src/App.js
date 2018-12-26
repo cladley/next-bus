29d3d9b5a16757277c39a414c1f1a6da00cc82b7
Colin Ladley <colinladley@hotmail.com>
2018-12-26 17:17:56 +0000

    Add routes for home, arrivals and map

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { loadUserRoutes } from "./actions";
 import styles from "./app.module.css";
 import MapView from "./views/MapView";
+import HomeView from "./views/HomeView";
+import ArrivalsView from "./views/ArrivalsView";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +17,9 @@ class App extends Component {
     return (
       <Router>
         <div className={styles["main-container"]}>
-          <MapView />
+          <Route exact path="/" component={HomeView} />
+          <Route path="/map" component={MapView} />
+          <Route path="/arrivals" component={ArrivalsView} />
         </div>
       </Router>
     );
---
+++ client/src/views/ArrivalsView.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ArrivalsView() {
+  return <div>Arrivals View</div>;
+}
