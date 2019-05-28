fa49dbf18bdd09cad9649d9183c7e1add2e662d7
Colin Ladley <ladleyc@FCCAM-DEVLPT09.uk.fc.local>
2019-05-28 17:18:23 +0100

    Add scroll item to route map

--- src/components/RouteList/index.js
+++ src/components/RouteList/index.js
@@ -1,24 +1,54 @@
 import React from "react";
 import styles from "./route-list.module.css";
+import {connect} from 'react-redux';
+import classNames from "classnames";
 
-const RouteListItem = () => {};
+
+const RouteListItem = React.forwardRef(({name, isCurrent}, ref) => {
+
+  const itemClassNames = classNames(styles.item, {
+    [styles.current]: isCurrent
+  });
+  
+  return (
+    <li className={itemClassNames} ref={ref}>
+      <span className={styles.name}>{name}</span>
+    </li>
+  );
+});
+
 
 class RouteList extends React.Component {
-  render() {
-    const { stops } = this.props;
+  selectedStopRef = React.createRef();
 
+  componentDidMount() {
+    this.selectedStopRef.current.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+  
+  render() {
+    const { stops, selectedStop } = this.props;
     return (
       <div className={styles["scroll-container"]}>
         <ul className={styles.list}>
-          {stops.map(stop => (
-            <li key={stop.id} className={styles.item}>
-              <span className={styles.name}>{stop.name}</span>
-            </li>
-          ))}
+          {stops.map(stop => {
+            if (stop.name === selectedStop.commonName) {
+              return <RouteListItem key={stop.id} name={stop.name} isCurrent={true} ref={this.selectedStopRef} />
+            } else {
+              return <RouteListItem key={stop.id} name={stop.name} isCurrent={stop.name === selectedStop.commonName} />
+            }
+          })}
         </ul>
       </div>
     );
   }
 }
 
-export default RouteList;
+const mapStateToProps = ({map, stops}) => {
+  return {
+    selectedStop: stops.byNaptanId[map.selectedStopId]
+  };
+};
+
+export default connect(mapStateToProps, null)(RouteList);
